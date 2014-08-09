4048fe5bc2896be28c84fcd8939d0bbeafbf5816
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-08-09 13:21:37 -0300

    Returns some gophers

--- gopher_endpoint.go
+++ gopher_endpoint.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 )
+
+type WombatResponse struct {
+	RequestId string   `json:"request_id"`
+	Gophers   []Gopher `json:"gophers"`
+}
+
+type Gopher struct {
+	Id int `json:"id"`
+}
 
 func main() {
 	log.Printf("Starting gopher_endpoint...")
@@ -21,7 +31,15 @@ func Root(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
-	io.WriteString(w, "{ \"gophers\": 0 }\n")
+	gophers := []Gopher{
+		{Id: 123}, {Id: 456}}
+
+	rootResponse := &WombatResponse{
+		RequestId: "123f",
+		Gophers:   gophers}
+
+	response, _ := json.Marshal(rootResponse)
+	io.WriteString(w, string(response))
 
 	log.Printf("/")
 }
