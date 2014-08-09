ffa81544d6e4624d93fe914ba9e484b589c7ecaf
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-08-09 15:09:31 -0300

    Returns request_id in response

--- gopher_endpoint.go
+++ gopher_endpoint.go
@@ -27,7 +27,22 @@ func main() {
 	}
 }
 
+func extractRequestId(r *http.Request) string {
+  defer r.Body.Close()
+
+  decoder := json.NewDecoder(r.Body)
+  var json map[string]interface{}
+  var err = decoder.Decode(&json)
+  if err != nil {
+    return "1234"
+  } else {
+    return json["request_id"].(string)
+  }
+}
+
 func Root(w http.ResponseWriter, r *http.Request) {
+  var requestId = extractRequestId(r)
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
@@ -35,11 +50,11 @@ func Root(w http.ResponseWriter, r *http.Request) {
 		{Id: 123}, {Id: 456}}
 
 	rootResponse := &WombatResponse{
-		RequestId: "123f",
+		RequestId: requestId,
 		Gophers:   gophers}
 
 	response, _ := json.Marshal(rootResponse)
 	io.WriteString(w, string(response))
 
-	log.Printf("/")
+  log.Printf("RequestId: %s\tPath: /", requestId)
 }
